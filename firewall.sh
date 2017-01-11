d9480c7477a6f866e85f6083f72298d10d422743
da99 <i-hate-spam-1234567@mailinator.com>
2017-01-11 15:08:35 -0600

    Dev. checkpoint.

--- firewall.sh
+++ firewall.sh
@@ -1,2 +1,3 @@
 ufw default deny incoming
 https://www.digitalocean.com/community/tutorials/how-to-setup-a-firewall-with-ufw-on-an-ubuntu-and-debian-cloud-server
+https://shadowd.zecure.org/overview/introduction/
